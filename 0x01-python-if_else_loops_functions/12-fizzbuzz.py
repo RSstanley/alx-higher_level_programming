0abb4a3bfab079eb5efafc054247a931e2793974
RSstanley <stanleyrudzani@gmail.com>
2023-06-06 22:49:00 +0200

    fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
@@ -8,4 +9,4 @@ def fizzbuzz():
         if i % 3 and i % 5:
             print("{:d}".format(i), end='')
         print(end=' ')
-        
+
