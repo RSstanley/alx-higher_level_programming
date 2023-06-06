5fd9020ca69a001ec2685d8e7908567ddd32201a
RSstanley <stanleyrudzani@gmail.com>
2023-06-06 20:23:59 +0000

    update 0 to 8

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # 5-print_comb2.py
 for anum in range(00, 100):
-    print("{:02d}".format(anum), end='\n' if anum == 99 else ", ") 
+    print("{:02d}".format(anum), end='\n' if anum == 99 else ", ")
